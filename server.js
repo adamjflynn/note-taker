f7706df5ca7499c1d3e9628699a7dbf4e75e4e0f
Adam Flynn <adamflynn1@gmail.com>
2021-11-15 19:08:35 -0600

    push to get heroku to work

--- server.js
+++ server.js
@@ -10,6 +10,26 @@ const app = express();
 
 
 
+// routes
+
+app.get('/', (rep, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get("/api/notes", function (req, res) {
+    fs.readFile("db/db.json", "utf8", function (err, notes) {
+        if (err) {
+            console.log(err)
+            return
+        }
+        res.json(JSON.parse(notes));
+    })
+});
+
 app.listen(PORT, () => {
     console.log('API server now live at http;//localhost:${PORT}!');
 });
