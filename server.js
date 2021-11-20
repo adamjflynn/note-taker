dcbf1f5cd695ec46a49bb66ee0a0aec7c58084a6
Adam Flynn <adamflynn1@gmail.com>
2021-11-20 16:24:54 -0600

    add function to delete notes

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // modules
 
-const express = require('express');
+const express = require("express");
 
-const fs = require('fs');
+const fs = require("fs");
 
-const path = require('path');
+const path = require("path");
 
 // port
 
@@ -14,54 +14,96 @@ const PORT = process.env.PORT || 3002;
 
 const app = express();
 
-app.use(express.urlencoded ( { extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 // routes
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+app.get("/notes", (req, res) => {
+	res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.get("/api/notes", function (req, res) {
-    fs.readFile("db/db.json", "utf8", function (err, notes) {
-        if (err) {
-            console.log(err)
-            return
-        }
-        res.json(JSON.parse(notes));
-    })
+	fs.readFile("db/db.json", "utf8", function (err, notes) {
+		if (err) {
+			console.log(err);
+			return;
+		}
+		res.json(JSON.parse(notes));
+	});
 });
 
 // function to post data to json
 
 app.post("/api/notes", function (req, res) {
-    const newNote = req.body
-    let notesDB = []
-    fs.readFile(path.join(__dirname + "/db/db.json"), "utf8", function (err, data) {
-        if (err) {
-            return console.log(err);
-        }
-        if (data === "") { // if starting from an empty json file
-            notesDB.push({ "id": 1, "title": newNote.title, "text": newNote.text });
-        } else {
-            notesDB = JSON.parse(data);
-            notesDB.push({ "id": notesDB.length + 1, "title": newNote.title, "text": newNote.text });
-        }
-        // updated notes pushed to db.json
-        fs.writeFile((path.join(__dirname + "/db/db.json")), JSON.stringify(notesDB), function (error) {
-            if (error) { return console.log(error); }
-            res.json(notesDB);
-        });
-    });
-  });
+	const newNote = req.body;
+	let notesDB = [];
+	fs.readFile(
+		path.join(__dirname + "/db/db.json"),
+		"utf8",
+		function (err, data) {
+			if (err) {
+				return console.log(err);
+			}
+			if (data === "") {
+				notesDB.push({ id: 1, title: newNote.title, text: newNote.text });
+			} else {
+				notesDB = JSON.parse(data);
+				notesDB.push({
+					id: notesDB.length + 1,
+					title: newNote.title,
+					text: newNote.text,
+				});
+			}
+			fs.writeFile(
+				path.join(__dirname + "/db/db.json"),
+				JSON.stringify(notesDB),
+				function (error) {
+					if (error) {
+						return console.log(error);
+					}
+					res.json(notesDB);
+				}
+			);
+		}
+	);
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+	const newNote = req.body;
+	const noteID = req.params.id;
+	let notesDB = [];
+	fs.readFile(
+		path.join(__dirname + "/db/db.json"),
+		"utf8",
+		function (err, data) {
+			if (err) {
+				return console.log(err);
+			}
+			notesDB = JSON.parse(data);
+			notesDB = notesDB.filter(function (object) {
+				return object.id != noteID;
+			});
+
+			fs.writeFile(
+				path.join(__dirname + "/db/db.json"),
+				JSON.stringify(notesDB),
+				function (error) {
+					if (error) {
+						return console.log(error);
+					}
+					res.json(notesDB);
+				}
+			);
+		}
+	);
+});
 
 app.listen(PORT, () => {
-    console.log(`API server now live at ${PORT}!`);
+	console.log(`API server now live at ${PORT}!`);
 });
