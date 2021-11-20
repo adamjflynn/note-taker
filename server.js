03b38c39aafbada4563623ec39e81e2f9163af21
Adam Flynn <adamflynn1@gmail.com>
2021-11-20 15:28:38 -0600

    fix formatting and add data parsing method

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
+// modules
+
 const express = require('express');
 
 const fs = require('fs');
 
 const path = require('path');
 
+// port
+
 const PORT = process.env.PORT || 3002;
+
+// server
 
 const app = express();
 
+app.use(express.urlencoded ( { extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
 
 
 // routes
 
-app.get('/', (rep, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
